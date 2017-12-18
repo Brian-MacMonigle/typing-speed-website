03b972ee2e24e4a914788d307ada5efeec9e0cbd
Brian-MacMonigle <brian.macmonigle@gmail.com>
2017-12-18 02:08:48 -0500

    init user-account-service

--- index.js
+++ index.js
@@ -6,48 +6,27 @@ const fetch = require('fetch-cookie')(require('node-fetch'));
 app.use(express.static(path.join(__dirname, 'page/build')));
 
 const api = 'https://brian-user-account-service.herokuapp.com/api';
-const initPost = {
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	method: 'post'
-}
 const pass = "7bc2e5d2-61e1-4e16-a04a-4ad8dee90f44";
 
 function login() {
-	return fetch(api + '/site/login', {	
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		method: 'post',
-		body: JSON.stringify({
-			site: 'typing-speed-website',
-			pass: pass
-		})
+	return fetch(api + '/login', {	
+		headers: {'Content-Type': 'application/json'},
+		method: 'POST',
+		body: JSON.stringify({user: 'typing-speed-website',	pass: pass})
 	})
-	.then(res => {
-		return res.json();
-	})
+	.then(res => res.json())
 	.then(json => {
-		if(json.Success === undefined) {
+		if(json.status === 'error') {
 			return Promise.reject(json);
 		}
-		console.log(json);
-	})
-	.then(() => {
-		fetch(api + "/site/login/clear");
 	})
 	.catch(err => {
 		console.log(err);
-		return fetch(api + '/site/create', {	
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			method: 'post',
-			body: JSON.stringify({
-				site: 'typing-speed-website',
-				pass: pass
-			})
+		// Try to create account
+		return fetch(api + '/create', {	
+			headers: {'Content-Type': 'application/json'},
+			method: 'POST',
+			body: JSON.stringify({user: 'typing-speed-website',	pass: pass})
 		})
 		.then(res => res.json())
 		.then(json => {
@@ -59,40 +38,37 @@ function login() {
 	})
 }
 
-login()
-.then(() => {
-	fetch(api + '/cookie')
-	.then(res => res.json())
-	.then(json => console.log(json))
-	.catch(err => console.log(err));
-});
+login();
+
+function saveIp(req, res, next) {
+	fetch(api + '/write/ip', {
+		headers: {'Content-Type': 'application/json'},
+		method: 'POST',
+		body: JSON.stringify({ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress, path: req.path})
+	})
+	next();
+}
+
+app.use(saveIp);
 
 app.get('/api/visitor', (req, res) => {
-	fetch(api + '/site/read', {
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		method: 'post',
-		body: JSON.stringify({name: 'visitor'})
-	})
+	fetch(api + '/read?path=visitor')
 	.then(res => res.json())
 	.then(val => {
-		let myPost = JSON.parse(JSON.stringify(initPost));
-		if(val === null) {
-			res.json({"visitor": 1})
-			myPost['body'] = JSON.stringify({
-				name: 'visitor',
-				value: 1
-			})
-		} else {
-			res.json({"visitor": val + 1});
-			myPost['body'] = JSON.stringify({
-				name: 'visitor',
-				value: val + 1
-			})
-		}
-
-		fetch(api + '/site/write', myPost)
+		val.message++;
+		res.json(val.message);
+		fetch(api + '/write', {
+			headers: {'Content-Type': 'application/json'},
+			method: 'POST',
+			body: JSON.stringify({path: 'visitor', data: val.message})
+		})
+		.then(res => res.json())
+		.then(val => {
+			if(val.status === 'error') {
+				return Promise.reject(val);
+			}
+			console.log(val);
+		})
 		.catch(err => console.log(err));
 	})
 	.catch(err => console.log(err));

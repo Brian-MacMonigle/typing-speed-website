7179e6cda846e4f565bebe39ccf1dc16729b704e
Brian-MacMonigle <brian.macmonigle@gmail.com>
2017-12-20 22:06:10 -0500

    Un-debugging mode

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const fetch = require('fetch-cookie')(require('node-fetch'));
 
 app.use(express.static(path.join(__dirname, 'page/build')));
 
-function saveIp(req, res, next) {
-	fetch(api + '/write/ip', {
-		headers: {'Content-Type': 'application/json'},
-		method: 'POST',
-		body: JSON.stringify({ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress, path: req.path})
-	})
-	.catch(err => console.log("Error: ", err));
-	next();
-}
-
-app.use(saveIp);
-
 const api = 'https://brian-user-account-service.herokuapp.com/api';
 const pass = "7bc2e5d2-61e1-4e16-a04a-4ad8dee90f44";
 var unique = 0;
@@ -34,6 +22,7 @@ fetch(api + '/login', {
 			return Promise.reject(json);
 		}
 	})
+	.then(() => fetch(api + '/login/clear'))
 	.catch(err => console.log(err));
 
 app.get('/api/visitor', (req, res) => {
@@ -41,8 +30,18 @@ app.get('/api/visitor', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.json({unique: unique, total: total});
-	//res.sendFile(__dirname + "page/build/index.html");
+	// fetch(api + '/write/ip', {
+	// 	headers: {'Content-Type': 'application/json'},
+	// 	method: 'POST',
+	// 	body: JSON.stringify({ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress, path: req.path})
+	// })
+	// .then(r => r.json())
+	// .then(r => {
+	// 	console.log(r);
+	// })
+	// .then(() => res.json({unique: unique, total: total}))
+	// .catch(err => res.send(err));
+	res.sendFile(__dirname + "page/build/index.html");
 });
 
 app.listen(process.env.PORT || 5000);
